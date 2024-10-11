8372d489dc4afeb02e9f63a6fb7255114767cdff
HassanKhan123 <hk260280@gmail.com>
2024-10-11 20:02:46 +0200

    added: booking feature

--- app/bookings/page.jsx
+++ app/bookings/page.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import Heading from "@/components/Heading";
+import BookedRoomCard from "@/components/BookedRoomCard";
+import getMyBookings from "../actions/getMyBookings";
 
-const BookingsPage = () => {
-  return <div>BookingsPage</div>;
+const BookingsPage = async () => {
+  const bookings = await getMyBookings();
+
+  return (
+    <>
+      <Heading title="My Bookings" />
+      {bookings.length === 0 ? (
+        <p className="text-gray-600 mt-4">You have no bookings</p>
+      ) : (
+        bookings.map((booking) => (
+          <BookedRoomCard key={booking.$id} booking={booking} />
+        ))
+      )}
+    </>
+  );
 };
 
 export default BookingsPage;
